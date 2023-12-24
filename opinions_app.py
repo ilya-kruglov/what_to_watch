d27e8ef697ea3fe604df005d52b2399d98b20f6c
banbao <ilya.kruglove@yandex.ru>
2023-12-24 16:23:46 +0300

    Handle 404 and 500 errors with custom templates

--- opinions_app.py
+++ opinions_app.py
@@ -54,6 +54,21 @@ class OpinionForm(FlaskForm):
     submit = SubmitField('Добавить')
 
 
+# Тут декорируется обработчик и указывается код нужной ошибки
+@app.errorhandler(404)
+def page_not_found(error):
+    # В качестве ответа возвращается собственный шаблон
+    # и код ошибки
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    # В таких случаях можно откатить незафиксированные изменения в БД
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index_view():
     # for key, value in app.config.items():
