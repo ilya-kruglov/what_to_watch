fb491106d6f99403d81ad86bc82192b4e5c3c805
banbao <ilya.kruglove@yandex.ru>
2023-12-24 17:00:23 +0300

    Added function load_opinions_command to load opinions from opinions.csv into the project's database using ORM. Used the click module for creating a custom command and the csv module for handling CSV files.

--- opinions_app.py
+++ opinions_app.py
@@ -1,3 +1,5 @@
+import csv
+import click
 from datetime import datetime
 from random import randrange
 from secrets import token_hex
@@ -131,6 +133,27 @@ def opinion_view(id):
     opinion = Opinion.query.get_or_404(id)
     # И передавать его в шаблон
     return render_template('opinion.html', opinion=opinion)
+
+
+@app.cli.command('load_opinions')
+def load_opinions_command():
+    """Функция загрузки мнений в базу данных."""
+    # Открывается файл
+    with open('opinions.csv', encoding='utf-8') as f:
+        # Создаётся итерируемый объект, который отображает каждую строку
+        # в качестве словаря с ключами из шапки файла
+        reader = csv.DictReader(f)
+        # Для подсчёта строк добавляется счётчик
+        counter = 0
+        for row in reader:
+            # Распакованный словарь можно использовать
+            # для создания объекта мнения
+            opinion = Opinion(**row)
+            # Изменения нужно зафиксировать
+            db.session.add(opinion)
+            db.session.commit()
+            counter += 1
+    click.echo(f'Загружено мнений: {counter}')
 
 
 if __name__ == '__main__':
