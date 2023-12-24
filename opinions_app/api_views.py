581cbf643acbda4daa2b268d544837dd3279491f
banbao <ilya.kruglove@yandex.ru>
2023-12-24 19:31:54 +0300

    Refactor API view serialization by introducing a 'to_dict' method in the 'Opinion' model, streamlining the code and enhancing maintainability

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -4,23 +4,10 @@ from . import app
 from .models import Opinion
 
 
-def opinion_to_dict(opinion):
-    return dict(
-        id=opinion.id,
-        title=opinion.title,
-        text=opinion.text,
-        source=opinion.source,
-        timestamp=opinion.timestamp,
-        added_by=opinion.added_by
-    )
-
-
 # Явно разрешить метод GET
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
     # Получить объект по id или выбросить ошибку
     opinion = Opinion.query.get_or_404(id)
-    # Конвертируем объект модели в словарь
-    data = opinion_to_dict(opinion)
     # Конвертировать данные в JSON и вернуть объект и код ответа API
-    return jsonify({'opinion': data}), 200
+    return jsonify({'opinion': opinion.to_dict()}), 200
